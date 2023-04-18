1134541d542d0a7f536e4b9327f426fc5cf58f21
Wilhelm <wilvo970@student.liu.se>
2023-04-18 10:11:03 +0200

    successfully serves static files via express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,20 @@
 // "typescript"
 
-const ETILE = 7331;
-const INDEX_PATH  = __dirname + '/public/index.html';
+const PORT : number = 7331;
 
 const express = require('express');
 const app = express();
-const port = ETILE;
+const path = require('path');   //    Path is used to handle paths for resources, 
+                                //     and can help in the way of e.g., detecting
+                                //     if a written path is faulty. Which wouldn't
+                                //     been as easy if it wasn't used.
 
-app.get('/', (req, res) => {
-    console.log("bara _____dir: " + __dirname);
-    res.sendFile(INDEX_PATH);
-});
-app.listen(port, () => {
+app.use( '/', express.static( path.join(__dirname, 'public') ) );
+
+// app.get('/', (req, res) => {
+//     console.log("bara _____dir: " + __dirname);
+// });
+
+app.listen(PORT, () => {
     console.log("We're listening!");
 });
