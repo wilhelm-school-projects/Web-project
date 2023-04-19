f0374069d20b45f591ba0791f8358417e5014ed9
Wilhelm <wilvo970@student.liu.se>
2023-04-19 19:00:46 +0200

    think I successfully migrated to sveltekit

--- website/src/montem/svelte.config.js
+++ website/src/montem/svelte.config.js
@@ -1,5 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// This might be useful if index isn't served to client!
+// 	
+//	You must ensure SvelteKit's
+//  trailingSlash option is set appropriately for your environment. If your host
+//  does not render /a.html upon receiving a request for /a then 
+//  you will need to set trailingSlash: 'always' to create /a/index.html instead.
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,12 +16,23 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+	// 	// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+	// 	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+	// 	adapter: adapter()
+	// }
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
