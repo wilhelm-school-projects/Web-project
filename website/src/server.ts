77df40766b7db7e14100be9f0eb1a08ac5e72d90
wilhelm <wilhelmvonka@outlook.com>
2023-04-25 17:07:37 +0200

    added dotenv to server side and client but client side doesn't work

--- website/src/server.ts
+++ website/src/server.ts
@@ -1,23 +1,24 @@
 // "typescript"
 
-const PORT : number = 7331;
-const INDEX_PATH    = __dirname + '/frontend/dist';
+const PORT: number = 7331;
+const INDEX_PATH = __dirname + '/frontend/dist';
 
+require('dotenv').config();
 const { exec } = require('child_process');  //  { exec } betyder att ta ut objectet (eller funktionen?) från ett object eller array. 
-                                            //  Det är alltså samma som "const exec = require('child_process').exec;"
+//  Det är alltså samma som "const exec = require('child_process').exec;"
 // const COMMAND       = 'ls ' + INDEX_PATH; 
 // const lsProcess = exec( COMMAND ); 
 // lsProcess.stdout.pipe(process.stdout);
-
+console.log(process.env.PASSWORD);
 
 const express = require('express');
 const app = express();
 const path = require('path');   //    Path is used to handle paths for resources, 
-                                //     and can help in the way of e.g., detecting
-                                //     if a written path is faulty. Which wouldn't
-                                //     been as easy if it wasn't used.
+//     and can help in the way of e.g., detecting
+//     if a written path is faulty. Which wouldn't
+//     been as easy if it wasn't used.
 
-app.use('/', express.static( path.join(__dirname, '/frontend/dist') ) );
+app.use('/', express.static(path.join(__dirname, '/frontend/dist')));
 
 app.listen(PORT, () => {
     console.log("We're listening!");
