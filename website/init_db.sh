361d8fb362d64735ee7e97db53cb34bd48e548e1
wilhelm <wilhelmvonka@outlook.com>
2023-04-27 09:11:12 +0200

    can connect to databas

--- website/init_db.sh
+++ website/init_db.sh
@@ -1,6 +1,9 @@
 # See for help: https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-22-04
 
 echo -e "____________ Hopefully this works ___________"
+echo "--------------------------------------------------------------"
+echo "Create password for root"
+echo "--------------------------------------------------------------"
 echo -e "run 'ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';'\nThen exit."
 sudo mysql --silent
 echo want to continue initialization \[Y/N\]?
@@ -11,7 +14,23 @@ if [ "$choice" != "Y" ] && [ "$choice" != "y" ]; then
     exit
 fi
 
-echo -e "\n Follow instructions provided from mysql_secure_installation"
-sudo mysql_secure_installation
-echo -e "run 'ALTER USER 'root'@'localhost' IDENTIFIED WITH auth_socket;'"
+echo "--------------------------------------------------------------"
+echo "Install mysql securely"
+echo "--------------------------------------------------------------"
+# echo -e "\n Follow instructions provided from mysql_secure_installation"
+# sudo mysql_secure_installation
+
+echo "--------------------------------------------------------------"
+echo "  Log in with auth_socket as root and create your user which will
+        be used by nodejs to access the database.
+--------------------------------------------------------------"
+echo "  run 'ALTER USER 'root'@'localhost' IDENTIFIED WITH auth_socket;'
+        USE mysql;
+        CREATE USER 'system_user'@'localhost' IDENTIFIED BY 'your password';
+        GRANT ALL PRIVILIGES ON *.* TO 'system_user'@'localhost';
+        FLUSH PRIVILIGES;
+        exit;
+"
 mysql -u root -p
+
+
--- website/src/server.ts
+++ website/src/server.ts
@@ -12,8 +12,8 @@ require('dotenv').config();
 const mysql = require('mysql2')
 const connection_values = {
     host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB
 }
 const connection = mysql.createConnection(connection_values);
