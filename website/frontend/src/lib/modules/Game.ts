87091673b8ba38a0815a42952d145660ae0044fb
wilhelm <wilhelmvonka@outlook.com>
2023-05-15 15:35:31 +0200

    --amend

--- website/frontend/src/lib/modules/Game.ts
+++ website/frontend/src/lib/modules/Game.ts
@@ -1,30 +1,35 @@
-function initCanvas(gameCanvas: HTMLCanvasElement): void {
-    // gameCanvas.width = window.innerWidth;
-    gameCanvas.height = window.innerHeight;
-    // window.addEventListener('resize', () => {
-    //     // gameCanvas.width = window.innerWidth;
-    //     gameCanvas.height = window.innerHeight;
-    // });
+function initCanvas(gameCanvas: HTMLCanvasElement, gamectx: CanvasRenderingContext2D): void {
+    const scale = window.devicePixelRatio;
+    gameCanvas.style.height = '{window.innerHeight}px';
+
+    gameCanvas.style.width = '{window.innerWidth}px';
+    gameCanvas.height = window.innerHeight * scale;
+    gameCanvas.width = window.innerWidth * scale;
+    gamectx.scale(scale, scale);
 }
 
 export class GameHost {
-    private gameCanvas: HTMLCanvasElement | null;
-    private gamectx: CanvasRenderingContext2DSettings;
+    private gameCanvas: HTMLCanvasElement;
+    private gamectx: CanvasRenderingContext2D;
 
     constructor(gameCanvas: string) {
         this.gameCanvas = document.getElementById(gameCanvas) as HTMLCanvasElement;
         if (this.gameCanvas == null) {
             throw new Error("Game Canvas is null!");
         }
-        this.gamectx = this.gameCanvas.getContext("2d") as CanvasRenderingContext2DSettings;
-        initCanvas(this.gameCanvas);
-    }
-    init(): void {
-        console.log("init");
+        this.gamectx = this.gameCanvas.getContext("2d") as CanvasRenderingContext2D;
+        initCanvas(this.gameCanvas, this.gamectx);
+
     }
 
     run(): void {
         console.log("run");
+        this.gamectx.fillStyle = "red";
+        this.gamectx.arc(200, 200, 50, 0, Math.PI * 2);
+        // this.gamectx.fillRect(0, 0, 30, 30);
+        console.log("height: " + this.gameCanvas.height);
+        console.log("width: " + this.gameCanvas.width);
+        this.gamectx.fill();
     }
 
     end(): void {
@@ -37,9 +42,6 @@ export class GameHost {
 }
 
 export class GameClient {
-    init(): void {
-        console.log("init");
-    }
 
     run(): void {
         console.log("run");
--- website/frontend/src/routes/game/[type]/+page.svelte
+++ website/frontend/src/routes/game/[type]/+page.svelte
@@ -2,17 +2,14 @@
 	import { page } from "$app/stores";
 	import { onMount } from "svelte";
 	import { getGameType, GameClient, GameHost } from "$lib/modules/Game";
-	let path: string = $page.url.href;
+	// extract the game type from the url
+	let gameType: string = $page.url.href
+		.split("/")
+		.at($page.url.href.split("/").length - 1)!;
 	let game: GameHost | GameClient;
 	onMount(() => {
-		// Choose game type based on what route was navigated to, game/host or game/client
-		game = getGameType(
-			$page.url.href.split("/").at($page.url.href.split("/").length - 1)!
-		);
-		console.log("onMount game/type: ");
-		console.dir(game);
+		game = getGameType(gameType);
 
-		game.init();
 		game.run();
 		game.end();
 	});
@@ -20,7 +17,7 @@
 
 <main class="game-container">
 	<div class="row">
-		<h1 class="text-center col">{path}</h1>
+		<h1 class="text-center col">{gameType}</h1>
 	</div>
 	<div class="row">
 		<canvas id="game-canvas"> hej </canvas>
@@ -38,9 +35,11 @@
 		width: 100vh; 
 		height: 100vh;
 	}  */
-	/* #game-canvas {
-		flex: auto;
-		width: 100%;
-		height: 100%;
-	} */
+	#game-canvas {
+		touch-action: none;
+		/* image-rendering: -moz-crisp-edges; */
+		/* image-rendering: -webkit-crisp-edges; */
+		/* image-rendering: pixelated; */
+		/* image-rendering: crisp-edges; */
+	}
 </style>
