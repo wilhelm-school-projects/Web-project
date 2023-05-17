40e8396277df84c0f0160b8a0588306445babbb4
wilhelm <wilhelmvonka@outlook.com>
2023-05-17 11:05:59 +0200

    restructured so classes for painting is now used

--- website/frontend/src/lib/modules/Game.ts
+++ website/frontend/src/lib/modules/Game.ts
@@ -1,76 +1,14 @@
-import { Rectangle, Circle } from '$lib/modules/Shape'
+import { Rectangle, Circle, Painter } from '$lib/modules/Shape_Handler'
 import { construct_svelte_component } from 'svelte/internal';
 
 class Game {
     gameCanvas: HTMLCanvasElement;
     gamectx: CanvasRenderingContext2D;
-    drawInterval: ReturnType<typeof setInterval>;
-    XposMouseCanvas: number;
-    YposMouseCanvas: number;
-    shapes: { [key: string]: any };
-    currentShape: string;
-
-    // Painting functions
-    // async draw(Event: MouseEvent) {
-    //     this.gamectx.beginPath();
-    //     this.gamectx.fillStyle = "rgba(150, 255, 140, 0.8)";
-    //     this.gamectx.arc(this.XposMouseCanvas, this.YposMouseCanvas, 10, 0, Math.PI * 2);
-    //     this.gamectx.closePath();
-    //     this.gamectx.fill();
-    // }
-    // initCanvas(gameCanvas: string): void {
-
-    //     // Eventlisteners for mouse
-    //     // this.gameCanvas.addEventListener('mousemove', (Event) => {
-    //     //     this.XposMouseCanvas = Event.clientX;
-    //     //     this.YposMouseCanvas = Event.clientY - this.gameCanvas.getBoundingClientRect().top;
-    //     // });
-
-    //     // this.gameCanvas.addEventListener('mousedown', (Event) => {
-    //     //     this.drawInterval = setInterval(() => this.draw(Event), 50);
-    //     //     console.log("mouse down");
-    //     // });
-
-    //     // this.gameCanvas.addEventListener('mouseup', () => {
-    //     //     clearInterval(this.drawInterval);
-    //     //     console.log("mouse up");
-    //     // });
-
-    //     // this.gameCanvas.addEventListener('mouseout', () => {
-    //     //     clearInterval(this.drawInterval);
-    //     //     console.log("mouse leaves canvas");
-    //     // });
-    // }
+    Painter: Painter;
 
     constructor(gameCanvas: string) {
-        this.gameCanvas = document.getElementById(gameCanvas) as HTMLCanvasElement;
-        if (this.gameCanvas == null) {
-            throw new Error("Game Canvas is null!");
-        }
-        this.gamectx = this.gameCanvas.getContext("2d") as CanvasRenderingContext2D;
-        this.gameCanvas.height = window.innerHeight;
-        this.gameCanvas.width = window.innerWidth;
+        this.Painter = new Painter(gameCanvas);
 
-        this.currentShape = "circle";
-        this.shapes = { rect: new Rectangle(this.gamectx, this.gameCanvas), circle: new Circle(this.gamectx, this.gameCanvas) };
-
-        // Event listeners for canvas
-        this.gameCanvas.addEventListener('mousedown', (Event) => {
-            // this.drawInterval = setInterval(() => this.shapes.at(this.currentShape).draw(Event), 50);
-            console.log(this.shapes[this.currentShape].draw(Event));
-            // this.drawInterval = setInterval(() => Object.entries(this.shapes)[this.currentShape], 50);
-            console.log("mouse down");
-        });
-
-        this.gameCanvas.addEventListener('mouseup', () => {
-            clearInterval(this.drawInterval);
-            console.log("mouse up");
-        });
-
-        this.gameCanvas.addEventListener('mouseout', () => {
-            clearInterval(this.drawInterval);
-            console.log("mouse leaves canvas");
-        });
     }
 
     run(): void {
@@ -89,10 +27,6 @@ export class GameHost extends Game {
 
     end(): void {
         console.log("end");
-    }
-
-    private dummy(): void {
-        console.log("private dummy");
     }
 }
 
---
+++ website/frontend/src/lib/modules/Shape_Handler.ts
@@ -0,0 +1,134 @@
+class Shape {
+    xPos: number;
+    yPos: number;
+    gamectx: CanvasRenderingContext2D;
+    gameCanvas: HTMLCanvasElement;
+
+    draw(): void {
+    }
+
+    constructor(xPosMouse: number, yPosMouse: number, gamectx: CanvasRenderingContext2D, gameCanvas: HTMLCanvasElement) {
+        this.gamectx = gamectx;
+        this.gameCanvas = gameCanvas;
+        this.xPos = xPosMouse;
+        this.yPos = yPosMouse;
+
+    }
+
+    setColor(rgba: number): void {
+        console.log("settings color: " + rgba);
+    }
+
+    hideMe(): void {
+        console.log("Hiding me :(");
+    }
+
+    displayMe(): void {
+        console.log("Displaing me :)");
+    }
+}
+
+export class Circle extends Shape {
+    radius: number;
+    endAngle: number = 2;
+
+    constructor(xPosMouse: number, yPosMouse: number, radius: number, endAngle: number, gamectx: CanvasRenderingContext2D, gameCanvas: HTMLCanvasElement) {
+        super(xPosMouse, yPosMouse, gamectx, gameCanvas);
+        this.radius = radius;
+        this.endAngle = endAngle;
+
+        this.draw();
+    }
+
+    draw() {
+        this.gamectx.beginPath();
+        this.gamectx.fillStyle = "rgba(150, 255, 140, 0.8)";
+        this.gamectx.arc(this.xPos, this.yPos, this.radius, 0, Math.PI * this.endAngle);
+        this.gamectx.closePath();
+        this.gamectx.fill();
+    }
+}
+
+export class Rectangle extends Shape {
+
+    draw() {
+    }
+}
+
+export class Painter {
+    xPosMouse: number;
+    yPosMouse: number;
+    drawInterval: ReturnType<typeof setInterval>;
+    gamectx: CanvasRenderingContext2D;
+    gameCanvas: HTMLCanvasElement;
+    currentShape = "circle";
+    shapes: { [key: string]: any };
+
+    constructor(gameCanvas: string) {
+        // Canvas and context
+        this.gameCanvas = document.getElementById(gameCanvas) as HTMLCanvasElement;
+        if (this.gameCanvas == null) {
+            throw new Error("Game Canvas is null!");
+        }
+        this.gamectx = this.gameCanvas.getContext("2d") as CanvasRenderingContext2D;
+        this.gameCanvas.height = window.innerHeight;
+        this.gameCanvas.width = window.innerWidth;
+
+        // Event listeners
+        this.gameCanvas.addEventListener('mousemove', (Event) => {
+            this.xPosMouse = Event.clientX;
+            this.yPosMouse = Event.clientY - this.gameCanvas.getBoundingClientRect().top;
+        });
+
+        this.gameCanvas.addEventListener('mousedown', (Event) => {
+            this.drawInterval = setInterval(() => this.draw(Event), 50);
+            console.log("mouse down");
+        });
+
+        this.gameCanvas.addEventListener('mouseup', () => {
+            clearInterval(this.drawInterval);
+            console.log("mouse up");
+        });
+
+        this.gameCanvas.addEventListener('mouseout', () => {
+            clearInterval(this.drawInterval);
+            console.log("mouse leaves canvas");
+        });
+
+        // Shapes and stuff
+        this.shapes = { rectangles: [], circles: [] };
+        this.currentShape = "circle";
+    }
+
+    draw(Event: MouseEvent) {
+        if (this.currentShape == "circle") {
+            this.shapes.circles.push(new Circle(this.xPosMouse, this.yPosMouse, 10, 2, this.gamectx, this.gameCanvas));
+        } else if (this.currentShape == "rectangle") {
+            this.shapes.rectangles.push(new Rectangle(this.xPosMouse, this.yPosMouse, this.gamectx, this.gameCanvas));
+        }
+        // console.dir(this.shapes);
+    }
+
+    informationToJSON(): string {
+        console.log("JSONifying all shape information and returns it");
+        return "";
+    }
+
+    loadContext(shapesJSON: string): void {
+        console.log("loading context")
+    }
+
+    updateContext(shapesJSON: string): void {
+        console.log("updating context");
+    }
+}
+
+
+
+
+
+
+
+
+
+
