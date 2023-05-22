08dd23d2ca1f1aa9149ea100085ffd8354a69a64
wilhelm <wilhelmvonka@outlook.com>
2023-05-22 19:26:44 +0200

    further work with sign in logic

--- website/frontend/src/routes/+page.svelte
+++ website/frontend/src/routes/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { slide, fade } from "svelte/transition";
-    import { fireAuth } from "$lib/modules/firebase";
-    import {
-        createUserWithEmailAndPassword,
-        signInWithEmailAndPassword,
-        type UserCredential,
-    } from "firebase/auth";
+    import { customFire } from "$lib/modules/firebase";
 
     //"FirebaseUI is not compatiable with Firebase SDk v9.0.0+ for now, currently it
     //is still dependent on Firebase v8. We're working on it right now, you can see
@@ -14,113 +9,10 @@
     //
     // Therefore I cannot use their simple login with google :(
 
-    let userEmail: string;
-    let password: string;
-    let isdisabled: boolean = false;
-    let signInBtn: HTMLButtonElement;
-    let signUpBtn: HTMLButtonElement;
-    let wrongCredentialsModal: HTMLElement;
-
-    function closeModal() {
-        wrongCredentialsModal.style.display = "none";
-    }
-
-    function showModal() {
-        wrongCredentialsModal.style.display = "block";
-    }
-
-    const validEmail = (email: string): boolean => {
-        if (
-            String(email)
-                .toLowerCase()
-                .match(
-                    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-                ) === null
-        ) {
-            return false;
-        }
-        return true;
-    };
-    async function signInUser() {
-        console.log("signin");
-        if (inputsAreEmpty()) {
-            return;
-        }
-        if (!validEmail(userEmail)) {
-            showModal();
-            console.log("not valid!");
-            return;
-        }
-
-        let userCredential: UserCredential | null;
-        try {
-            let userCredentials = await signInWithEmailAndPassword(
-                fireAuth,
-                userEmail,
-                password
-            );
-            console.log(userCredentials);
-        } catch (e) {
-            showModal();
-            console.log(e);
-        }
-    }
-    async function signUpUser() {
-        console.log("signUp");
-        if (inputsAreEmpty()) {
-            return;
-        }
-        if (!validEmail(userEmail)) {
-            showModal();
-            console.log("not valid!");
-            return;
-        }
-        try {
-            let userCredentials = await createUserWithEmailAndPassword(
-                fireAuth,
-                userEmail,
-                password
-            );
-            console.log(userCredentials);
-        } catch (e) {
-            console.log(e);
-        }
-    }
-    function initiateHTMLElements() {
-        signInBtn = document.getElementById(
-            "button-signin"
-        ) as HTMLButtonElement;
-
-        signUpBtn = document.getElementById(
-            "button-signin"
-        ) as HTMLButtonElement;
-
-        wrongCredentialsModal = document.getElementById(
-            "modal-wrong-credentials"
-        ) as HTMLButtonElement;
-    }
-
-    function inputsAreEmpty() {
-        if (userEmail === "" || password === "") {
-            return true;
-        }
-        return false;
-    }
     onMount(async () => {
         localStorage.setItem("email", "pinto@pinto.se");
-        initiateHTMLElements();
+        customFire.initiateHTMLElements();
     });
-
-    // signInWithEmailAndPassword(auth, email, password)
-    //     .then((userCredential) => {
-    //         // Signed in
-    //         const user = userCredential.user;
-    //         // ...
-    //     })
-    //     .catch((error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //     });
 </script>
 
 <!-- transition:slide={{ axis: 'x', duration: 700 }} -->
@@ -140,7 +32,7 @@
                     <label for="userEmail"> Email: </label>
                     <input
                         name="userEmail"
-                        bind:value={userEmail}
+                        bind:value={customFire.userEmail}
                         type="email"
                         class=" rounded"
                     />
@@ -149,7 +41,7 @@
                     <label for="password"> Password: </label>
                     <input
                         name="password"
-                        bind:value={password}
+                        bind:value={customFire.password}
                         type="password"
                         class=" rounded"
                     />
@@ -160,22 +52,12 @@
         <div id="right-col" class="col-2 border" />
     </div>
     <div class="row">
-        <button
-            id="button-signin"
-            class="col btn btn-primary"
-            on:click={signInUser}
-        >
+        <button id="button-signin" class="col btn btn-primary">
             Sign in
         </button>
     </div>
     <div class="row">
-        <button
-            id="button-signup"
-            class="col btn btn-danger"
-            on:click={signUpUser}
-        >
-            Sign up
-        </button>
+        <button id="button-signup" class="col btn btn-danger"> Sign up </button>
     </div>
 
     <div id="modal-wrong-credentials" class="modal" tabindex="-1">
@@ -191,7 +73,7 @@
                         class="btn btn-primary"
                         data-bs-dismiss="modal"
                         aria-label="Close"
-                        on:click={closeModal}
+                        on:click={customFire.closeModal}
                     >
                         X
                     </button>
